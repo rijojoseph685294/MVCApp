834d17ec0b395ea8ec3ab826ce29627b5c4175e2
Admin <Admin@DOTNET>
2018-12-13 18:35:19 +0530

    app start files

---
+++ FSD Assignments/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FSD_Assignments
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
