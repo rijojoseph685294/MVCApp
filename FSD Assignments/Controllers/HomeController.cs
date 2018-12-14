62e6b778dc62ac802748c6ba079f965af241732b
Admin <Admin@DOTNET>
2018-12-14 11:25:13 +0530

    test

--- FSD Assignments/Controllers/HomeController.cs
+++ FSD Assignments/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace FSD_Assignments.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View();   
         }
 
         public ActionResult About()
